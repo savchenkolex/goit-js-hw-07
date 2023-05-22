0abc2ff87d70c134b79b2c7857f192e76187cac7
lex <savchenko.lex@gmail.com>
2023-05-23 00:31:56 +0300

    02-simple lightbox done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML("beforeend",createGalleryItems(galleryItems));
+
+// galleryEl.addEventListener("click", imageClickGalleryHendler);
+
+function createGalleryItems(arr) {
+    return arr.map(({preview, original,description})=>{
+
+        return `<li class="gallery__item" >
+                    <a href="${original}" class="gallery__link">
+                        <img 
+                        class="gallery__image" 
+                        src="${preview}" 
+                        data-original="${original}" 
+                        alt="${description}" 
+                        width=300>
+                    </a>
+                </li>`;} ).join("");
+}
+
+let lightbox = new SimpleLightbox('.gallery a');
