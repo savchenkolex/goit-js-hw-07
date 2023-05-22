f6626e7bcd0d3e869c419880e72659d105ffe25e
lex <savchenko.lex@gmail.com>
2023-05-22 23:11:26 +0300

    01-gallery + esc = done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.addEventListener("click", imageClickGalleryHendler);
+
+galleryEl.insertAdjacentHTML("beforeend",createGalleryItems(galleryItems));
+
+document.addEventListener("keydown", keyCloseImgHandler);
+
+
+let instance = {};
+
+function createGalleryItems(arr) {
+    return arr.map(({preview, original,description})=>{
+        return `<li class="gallery__item" >
+                    <a href="#" class="gallery__link">
+                        <img 
+                        class="gallery__image" 
+                        src="${preview}" 
+                        data-original="${original}" 
+                        alt="${description}" 
+                        width=300>
+                    </a>
+                </li>`;} ).join("");
+}
+
+function imageClickGalleryHendler (event) {
+    const targetEl = event.target;
+    if (!targetEl.classList.contains("gallery__image")) {
+        return;
+    }
+    const title = targetEl.alt;
+    const bigImgURL = targetEl.dataset.original;
+    instance = basicLightbox.create(`
+	<h2 class="gallery-title">${title}</h2>
+	<img class="gallery__image" src="${bigImgURL}" alt="${title}" >
+    `);
+
+    instance.show();
+    
+}
+
+function keyCloseImgHandler (event) {
+    const isEscape = event.code === "Escape";
+    
+    if ( !isEscape) {
+        return;
+    }
+    if (basicLightbox.visible()) {
+        instance.close();
+    }
+   
+}
+
